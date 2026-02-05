f17850e041d43ed6b70b4b3e032be4ef3d08c869
bowook <mbosx316@naver.com>
2026-02-05 17:46:38 +0900

    chore: initial Spring Boot project setup

---
+++ src/main/java/com/recokr/internship/crawler/BiddingCrawlerApplication.java
@@ -0,0 +1,13 @@
+package com.recokr.internship.crawler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BiddingCrawlerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BiddingCrawlerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/recokr/internship/crawler/BiddingCrawlerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.recokr.internship.crawler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BiddingCrawlerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
